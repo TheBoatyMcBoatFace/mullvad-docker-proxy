749123bab34d4a1ae1388fd77fcce2bcbf7ace54
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-12 21:52:06 +0000

    Primary IP Dynamic Set

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,13 @@ cp "/etc/wireguard/profiles/${PROFILE}" /etc/wireguard/wg0.conf
 sed -i "s|PrivateKey_PLACEHOLDER|${PRIVATE_KEY}|g" /etc/wireguard/wg0.conf
 
 # Set up the Wireguard interface
-WG_QUICK_RESOLVCONF=/sbin/openresolv wg-quick up wg0
+wg-quick up wg0
+
+# Get the primary IP address of the Docker container
+PRIMARY_IP=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n 1)
+
+# Update the squid.conf file with the primary IP address
+sed -i "s|tcp_outgoing_address .*|tcp_outgoing_address ${PRIMARY_IP}|g" /etc/squid/squid.conf
 
 ln -sf /dev/stdout /var/log/squid/access.log && \
 ln -sf /var/log/squid/access.log /squid-logs/access.log
