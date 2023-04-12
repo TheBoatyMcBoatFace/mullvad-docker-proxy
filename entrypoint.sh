ec4cde0fa4fa835ec8a090ecb15542ae4a107466
Bentley Hensel <bentley.hensel@civicactions.com>
2023-04-12 19:46:52 +0000

    I’m an idiot…

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "Using profile: ${PROFILE}"
 
 # Copy the selected profile to wg0.conf and replace the private key placeholder
 cp "/etc/wireguard/profiles/${PROFILE}" /etc/wireguard/wg0.conf
-sed -i "s|PrivateKey_PLACEHOLDER|PrivateKey = ${PRIVATE_KEY}|g" /etc/wireguard/wg0.conf
+sed -i "s|PrivateKey_PLACEHOLDER|${PRIVATE_KEY}|g" /etc/wireguard/wg0.conf
 
 # Set up the Wireguard interface
 wg-quick up wg0
